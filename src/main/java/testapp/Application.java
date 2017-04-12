d5f3d6df24a7160951df55e33f797ecda9224a95
Ervin Fetic <ervin@rateboard.info>
2017-04-12 10:23:12 +0200

    Changed source code of main class. Added specific modelmapper configuration that will make this example to work.

--- src/main/java/testapp/Application.java
+++ src/main/java/testapp/Application.java
@@ -2,35 +2,45 @@ package testapp;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.NamingConventions;
 import testapp.generated.ota2008a.OTAHotelResNotifRQ_2008A;
 import testapp.generated.ota2008a.POSType_2008A;
 import testapp.generated.ota2008a.SourceType_2008A;
 import testapp.generated.ota2015b.OTAHotelResNotifRQ_2015B;
 
+/* 
+ *  @description This is example of bug that I had with ModelMapper library. If you want to make this example to work you have to uncomment the below configuration 
+ *  otherwise your source code will not be mapped due to nested collections. Please
+ *  try either with or without the following configuration below.
+ *  
+ *  @author Ervin
+ *  @created 10/04/2017
+ *  @modified 12/04/2017
+ */
 public class Application {
 
 	public static void main(String[] args) {
 		
 		ModelMapper modelMapper = new ModelMapper();
-
-		/*
-		I found somewhere if I have no setter methods for a lists I can specify below configuration 
-		and it should work event without setters but it did not work. You can also try with this configuration
-		and you'll see an error that I got.*/
 		
-//		modelMapper.getConfiguration().setFieldMatchingEnabled(true);
-//		modelMapper.getConfiguration().setMethodAccessLevel(AccessLevel.PUBLIC);
-//		modelMapper.getConfiguration().setFieldAccessLevel(AccessLevel.PROTECTED);
-		
+		//modelMapper.getConfiguration().setFieldMatchingEnabled(true);
+		//modelMapper.getConfiguration().setFieldAccessLevel(AccessLevel.PROTECTED);
+		//modelMapper.getConfiguration().setSourceNamingConvention(NamingConventions.JAVABEANS_MUTATOR);
+		 
 		// Fills the source item
 		OTAHotelResNotifRQ_2008A item1 = new OTAHotelResNotifRQ_2008A();
 		
-		SourceType_2008A source = new SourceType_2008A();
-		source.setTerminalID("Terminal-001");
-		source.setISOCountry("UK");
+		SourceType_2008A source1 = new SourceType_2008A();
+		source1.setTerminalID("Terminal-001");
+		source1.setISOCountry("UK");
+		
+		SourceType_2008A source2 = new SourceType_2008A();
+		source2.setTerminalID("Terminal-002");
+		source2.setISOCountry("GE");
 		
 		POSType_2008A pos = new POSType_2008A();
-		pos.getSource().add(source);
+		pos.getSource().add(source1);
+		pos.getSource().add(source2);
 		
 		item1.setPOS(pos);
 		item1.setEchoToken("My echo token!");
@@ -39,18 +49,24 @@ public class Application {
 		OTAHotelResNotifRQ_2015B item2 = modelMapper.map(item1, OTAHotelResNotifRQ_2015B.class);
 		
 		if (item2 != null) {
-			// The POS variable wont be mapped because of list
+				
 			if (item2.getPOS() != null) {
 				if (item2.getPOS().getSource() != null && item2.getPOS().getSource().size() > 0) {
 					
-					// Prints the terminal id
 					if (item2.getPOS().getSource().get(0).getTerminalID() != null) {
-						System.out.println("Terminal: " + item2.getPOS().getSource().get(0).getTerminalID());
+						System.out.println("Terminal1: " + item2.getPOS().getSource().get(0).getTerminalID());
 					}
 					
-					// Prints the country code
 					if (item2.getPOS().getSource().get(0).getISOCountry() != null) {
-						System.out.println("Country: " + item2.getPOS().getSource().get(0).getISOCountry());
+						System.out.println("Country1: " + item2.getPOS().getSource().get(0).getISOCountry());
+					}
+					
+					if (item2.getPOS().getSource().get(1).getTerminalID() != null) {
+						System.out.println("Terminal2: " + item2.getPOS().getSource().get(1).getTerminalID());
+					}
+					
+					if (item2.getPOS().getSource().get(1).getISOCountry() != null) {
+						System.out.println("Country2: " + item2.getPOS().getSource().get(1).getISOCountry());
 					}
 					
 				} else {
